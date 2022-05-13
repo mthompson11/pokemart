e0636d0995ca5dc1fc830f73a7600e76e3386afd
mthompson11 <mthompson@Michaels-MacBook-Pro.local>
2022-05-13 12:53:29 -0700

    finished logout and better navigation

--- src/pages/PaymentSuccessPage/PaymentSuccessPage.jsx
+++ src/pages/PaymentSuccessPage/PaymentSuccessPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect} from "react";
 import * as ordersAPI from '../../utilities/orders-api'
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function PaymentSuccessPage({ cart, setCart }){
     
@@ -8,16 +9,17 @@ export default function PaymentSuccessPage({ cart, setCart }){
         async function markCartPaid(){
             await ordersAPI.markCartPaid()
             const newCart = await ordersAPI.getCart();
-            // eslint-disable-next-line
             setCart(newCart);
         };
         markCartPaid();
       }, []);
+
+    const navigate = useNavigate()
     
     return(
         <>
             <h1>Payment Success!</h1>
-            <Button variant='contained' sx={{ borderRadius: 10 }}>
+            <Button variant='contained' sx={{ borderRadius: 10 }} onClick={() => navigate('/')}>
                 Keep Shopping!
             </Button>
         </>
